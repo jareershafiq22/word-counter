53e6abbe8796f0f79258be3809e4d776c112a327
jareershafiq22 <165843803+jareershafiq22@users.noreply.github.com>
2024-05-18 07:47:38 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+// Importing inquirer and chalk Packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Display a colorfull Welcome massage
+console.log(chalk.bold.cyanBright ("\n  \t\t Welcome to Jareer Shafiq - Word counter"));
+console.log("=".repeat(60));
+
+//Prompt the user to enter a Sentence
+let answers = await inquirer.prompt([
+     {
+        name: "sentence",
+        type: "input",
+        message:  "Enter a Sentence",
+     }
+]);
+
+//Trimming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+
+//Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(` \n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
